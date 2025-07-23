0e6c56e7935129beca6c7d9c6a43cbe5b7e7b513
alinazh <siberiangodness@mail.ru>
2025-07-23 22:27:03 +0300

    Add new args to prompt bulding for every universal agent

--- protollm/agents/agent_prompts.py
+++ protollm/agents/agent_prompts.py
@@ -144,34 +144,46 @@ def build_replanner_prompt(
 def build_supervisor_prompt(
     scenario_agents: list = ["web_search", "chemist", "nanoparticles", "automl"],
     tools_for_agents: dict = {"web_search": ["TavilySearchResults"]},
-    last_memory: str = "",
+    last_memory: str = "", problem_statement = None, problem_statement_continue = None, rules = None, 
+    additional_rules = None, examples = None, enhancemen_significance = None
 ) -> ChatPromptTemplate:
     tools_descp_for_agents = ""
     for agent, tools in tools_for_agents.items():
         tools_descp_for_agents += f"- {agent} has tools: {', '.join(tools)}\n"
-
-    supervisor_system_prompt = (
-        "You are a supervisor managing a team of specialized workers. "
-        f"Available workers: {', '.join(scenario_agents)}\n\n"
-        "Your task is to assign workers to execute the next step in a multi-step plan.\n\n"
-        "Rules:\n"
+        
+    problem_statement = problem_statement or "You are a supervisor managing a team of specialized workers. "
+    problem_statement_continue = problem_statement_continue or "Your task is to assign workers to execute the next step in a multi-step plan.\n\n"
+    rules = rules or (
         "1. If the step contains only one task, assign one appropriate worker.\n"
         "2. If the step contains multiple parallel tasks (i.e. list of tasks), assign the same number of appropriate workers.\n"
         "   - Each task must have one worker\n"
         "   - All assigned workers must be able to handle their respective tasks based on their tools and capabilities.\n"
         "3. If no workers are needed (all tasks complete), return an empty list.\n"
         "4. Consider worker capabilities and tools:\n"
-        f"{tools_descp_for_agents}\n\n"
+        )
+    additional_rules = additional_rules or (
         "Output MUST be in JSON format with the key 'next' containing a list of workers.\n"
         "The number of workers MUST match the number of tasks in the current step.\n\n"
-        "Example outputs:\n"
+        )
+    examples = examples or (     
         '{{"next": ["a_worker", "b_worker", "c_worker"]}}\n'
         '{{"next": ["web_search", "chemist"]}}\n'
         '{{"next": ["automl"]}}\n'
         '{{"next": []}}\n\n'
-        "You must follow the format!!! Always return json!\n"
+        )
+    enhancemen_significance = enhancemen_significance or "You must follow the format!!! Always return json!\n"
+    
+    supervisor_system_prompt = (problem_statement + f"Available workers: {', '.join(scenario_agents)}\n\n" \
+        + problem_statement_continue +\
+        "Rules:\n" + rules +\
+        f"{tools_descp_for_agents}\n\n" + additional_rules +\
+        "Example outputs:\n" +\
+        examples +\
+        enhancemen_significance +\
         "Previous conversation context:\n"
         f"{last_memory}\n\n"
+        "{format_instructions}"
+        
         "User request: {input}"
     )
 
@@ -183,46 +195,59 @@ def build_supervisor_prompt(
 worker_prompt = "You are a helpful assistant. You can use provided tools. \
     If there is no appropriate tool, or you can't use one, answer yourself"
 
+def build_summary_prompt(additional_hints = "", problem_statement = None, rules=None):
+    problem_statement = problem_statement or """Your task is to formulate final answer based on system_response using 
+        intermediate_thoughts to make sure user gets full answer to their query."""
+    rules = rules or """Your response must be direct answer to user query, don't write too much text. 
+        You can use phrases like: it have been done, etc. Instead you must directly 
+        tell what have been done, 
+        extract all important results
+        You should respond in markdown format. MAKE SURE YOUR RESPONSE IS THE ANSWER TO THE USER QUERY
+        You must double check that your respond is the answer to user query."""
+        
+    summary_prompt = ChatPromptTemplate.from_template(
+            """{problem_statement}
+        {rules}
 
-summary_prompt = ChatPromptTemplate.from_template(
-    """Your task is to formulate final answer based on system_response using 
-intermediate_thoughts to make sure user gets full answer to their query.
-Your response must be direct answer to user query, don't write too much text. 
-You can use phrases like: it have been done, etc. Instead you must directly 
-tell what have been done, 
-extract all important results
-You should respond in markdown format. MAKE SURE YOUR RESPONSE IS THE ANSWER TO THE USER QUERY
-You must double check that your respond is the answer to user query.
 
+        Your objective is this:
+        User query: {query};
+        System_response: {system_response};
+        intermediate_thoughts: {intermediate_thoughts};
+        """ + additional_hints
+        ).partial(problem_statement=problem_statement, rules=rules)
+    return summary_prompt
 
-Your objective is this:
-User query: {query};
-System_response: {system_response};
-intermediate_thoughts: {intermediate_thoughts};
-"""
-)
-
-chat_prompt = ChatPromptTemplate.from_template(
+def build_chat_prompt(problem_statement = None, additional_hints_for_scenario_agents = None):
+    problem_statement = problem_statement or """
+    Now, the given objective, check whether it is simple enough to answer yourself. \
+    If you can answer without any help and tools and the question is simple inquery, then write your answer. If you can't do that, call next worker: planner
+    If the question is related to running models or checking for presence, training, inference - call planer!
+    You should't answer to a several-sentenced questions. You can only chat with user on a simle topics.
     """
-Here is what the user and system previously discussed:
-{last_memory}
 
-Now, the given objective, check whether it is simple enough to answer yourself. \
-If you can answer without any help and tools and the question is simple inquery, then write your answer. If you can't do that, call next worker: planner
-If the question is related to running models or checking for presence, training, inference - call planer!
-You should't answer to a several-sentenced questions. You can only chat with user on a simle topics.
+    chat_prompt = ChatPromptTemplate.from_template(
+        """
+    Here is what the user and system previously discussed:
+    {last_memory}
 
-If a user asks about your capabilities, tell him something from this:
-{additional_hints_for_scenario_agents}
+    {problem_statement}
 
-Your objective is this:
-{input}
+    If a user asks about your capabilities, tell him something from this:
+    {additional_hints_for_scenario_agents}
 
-Your output should match this JSON format, don't add any intros!!! It is important!
-{{
-  "action": {{
-    "next" | "response" : str | str
-  }}
-}}
-"""
-).partial(format_instructions=chat_parser.get_format_instructions())
+    Your objective is this:
+    {input}
+
+    Your output should match this JSON format, don't add any intros!!! It is important!
+    {{
+    "action": {{
+        "next" | "response" : str | str
+    }}
+    }}
+    """
+    ).partial(problem_statement=problem_statement,
+              additional_hints_for_scenario_agents=additional_hints_for_scenario_agents,
+              format_instructions=chat_parser.get_format_instructions()
+              )
+    return chat_prompt
